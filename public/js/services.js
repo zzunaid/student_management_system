1af177b66f5128008e56a02476d9b21750021703
Zunaid <mailtozunaid@gmail.com>
2016-06-20 14:43:41 +0530

    adds herouku server endpoints

--- public/js/services.js
+++ public/js/services.js
@@ -4,27 +4,27 @@ angular.module('F1FeederApp.services', [])
     var ergastAPI = {};
 
     ergastAPI.getRosters = function() {
-      return $http.get("http://localhost:3000/rosters");
+      return $http.get("https://evening-shelf-96900.herokuapp.com/rosters");
     }
 
     ergastAPI.getStudents = function(id) {
-      return $http.get("http://localhost:3000/students?roster_id="+id)
+      return $http.get("https://evening-shelf-96900.herokuapp.com/students?roster_id="+id)
     }
 
     ergastAPI.getStudent = function(id) {
-      return $http.get("http://localhost:3000/students/"+id)
+      return $http.get("https://evening-shelf-96900.herokuapp.com/students/"+id)
     }
 
     ergastAPI.deleteStudent = function(id) {
-      return $http.delete("http://localhost:3000/students/"+id)
+      return $http.delete("https://evening-shelf-96900.herokuapp.com/students/"+id)
     }
 
     ergastAPI.updateStudent = function(id, data) {
-      return $http.put("http://localhost:3000/students/"+id, data)
+      return $http.put("https://evening-shelf-96900.herokuapp.com/students/"+id, data)
     }
 
     ergastAPI.createStudent = function(id, data) {
-      return $http.post("http://localhost:3000/students/", data)
+      return $http.post("https://evening-shelf-96900.herokuapp.com/students/", data)
     }
 
     ergastAPI.getDriverRaces = function(id) {
